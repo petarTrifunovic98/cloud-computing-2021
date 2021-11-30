2c0316044063049d3e9a10b3aa23ff62e647db66
Petar <peki.trifunovic@gmail.com>
2021-11-30 23:27:27 +0100

    Counter incrementing fixed

--- cloud_project/counter/views.py
+++ cloud_project/counter/views.py
@@ -8,6 +8,7 @@ def increase_counter(request):
     if len(all_counters) == 0:
         c = Counter()
     else:
+        all_counters = all_counters.order_by('id')
         c = all_counters[0]
     c.counter += 1
     c.save()

2beda11af460b25ff3b4a232a6ec4836c1f5fad1
Petar <peki.trifunovic@gmail.com>
2022-01-19 12:56:31 +0100

    Switched to the gunicorn server

--- web_app/run_server.sh
+++ web_app/run_server.sh
@@ -4,4 +4,5 @@ cd ./cloud_project
 poetry run python manage.py makemigrations
 poetry run python manage.py migrate
 # poetry run python manage.py createsuperuser
-poetry run python manage.py runserver 0.0.0.0:8000
+# poetry run python manage.py runserver 0.0.0.0:8000
+gunicorn cloud_project.wsgi:application --bind 0.0.0.0:8000

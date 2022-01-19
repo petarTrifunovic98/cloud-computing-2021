cf9a3c585c561d3625086baa3fa10a777bf05372
Petar <peki.trifunovic@gmail.com>
2022-01-19 13:22:29 +0100

    Updated run_server.sh script for web_app

--- web_app/run_server.sh
+++ web_app/run_server.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-cat pyproject.toml
 cd ./cloud_project
 poetry run python manage.py makemigrations
 poetry run python manage.py migrate
 # poetry run python manage.py createsuperuser
 # poetry run python manage.py runserver 0.0.0.0:8000
-gunicorn cloud_project.wsgi:application --bind 0.0.0.0:8000
+poetry run gunicorn cloud_project.wsgi:application --bind 0.0.0.0:8000
